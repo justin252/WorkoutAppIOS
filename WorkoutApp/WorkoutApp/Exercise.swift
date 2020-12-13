abcef205d10975e0b4c65cd10ed144b15fc8f4e5
ethanstanley <49345412+ethanstanley@users.noreply.github.com>
2020-12-13 12:25:56 -0500

    Update Exercise.swift

--- WorkoutApp/WorkoutApp/Exercise.swift
+++ WorkoutApp/WorkoutApp/Exercise.swift
@@ -11,11 +11,13 @@ class Exercise {
     var imageName: String
     var name: String
     var muscleTarget: String
+    var seen: Bool
     
-    init(imageName: String, name: String, muscleTarget: String) {
+    init(imageName: String, name: String, muscleTarget: String, seen: Bool) {
         self.name = name
         self.muscleTarget = muscleTarget
         self.imageName = imageName
+        self.seen = seen
     }
     
     func setName(name: String){
