88b7e404f33933b44c95bd90b9767e7a4164a202
Tucker Stanley <tucker.stanley@gmail.com>
2020-12-13 11:49:27 -0500

    Update Exercise.swift

--- WorkoutApp/WorkoutApp/Exercise.swift
+++ WorkoutApp/WorkoutApp/Exercise.swift
@@ -7,9 +7,25 @@
 
 import Foundation
 
-struct Exercise: Equatable {
+class Exercise {
     var imageName: String
     var name: String
     var muscleTarget: String
+    
+    init(imageName: String, name: String, muscleTarget: String) {
+        self.name = name
+        self.muscleTarget = muscleTarget
+        self.imageName = imageName
+    }
+    
+    func setName(name: String){
+        self.name = name
+    }
+    func setMuscleTarget(muscleTarget: String){
+        self.muscleTarget = muscleTarget
+    }
+    func setImageName(imageName: String){
+        self.imageName = imageName
+    }
 }
 
